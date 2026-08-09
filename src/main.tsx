6e3a7202393b7c583d19671292afb3773620d01e
successpartner10 <successpartner10@github.com>
2026-08-09 19:57:33 +0000

    Fix: relative asset paths in index.html + SW path

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 // Register service worker for PWA
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/photo/sw.js').catch(() => {});
+  navigator.serviceWorker.register('./sw.js').catch(() => {});
 }
 
 createRoot(document.getElementById('root')!).render(
